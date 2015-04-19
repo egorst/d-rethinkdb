07c245ea31891533b6b6cf566cd2d2fa19ec2e01
Egor Starostin <egorst@gmail.com>
2015-04-19 23:52:33 +0300

    another simple version of connect()

--- rethinkdb.d
+++ rethinkdb.d
@@ -1,17 +1,23 @@
 module rethinkdb;
 
-import std.socket, std.string, std.conv, std.stream, std.socketstream, std.stdio;
+import std.socket, std.string, std.conv, std.stdio;
 
-struct Rethinkdb {
+class Rethinkdb {
 
-	string rethinkdb_host;
-	ushort rethinkdb_port;
-	Stream rethinkdb_connection;
+	struct Connection {
+		Socket sock;
+	}
 	
-	public void connect(string domain, ushort port) {
-		Socket rdb = new TcpSocket(new InternetAddress(domain,port));
-		scope (exit) rdb.close();
-		rethinkdb_connection = new SocketStream(rdb);
+	public Connection connect(string host, ushort port, string db = "" , string auth_key = "", int timeout = 0) {
+		Connection c;
+		c.sock = new Socket(AddressFamily.INET, SocketType.STREAM);
+		scope (exit) c.sock.close();
+		c.sock.connect(new InternetAddress(host,port));
+		c.sock.send("\x20\x2d\x0c\x40\x00\x00\x00\x00\xc7\x70\x69\x7e");
+		char[1024] buf;
+		auto rcv = c.sock.receive(buf);
+		writefln("server said %d bytes: %s",rcv,buf[0..rcv]);
+		return c;
 	}
 }
 
