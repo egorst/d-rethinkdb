0878481d0b8b2184a3ba59146b02259912df1a5e
Egor Starostin <egorst@gmail.com>
2015-04-23 19:44:05 +0300

    trivial changes

--- rethinkdb.d
+++ rethinkdb.d
@@ -7,11 +7,13 @@ class Rethinkdb {
 	struct Connection {
 		Socket sock;
 	}
+
+	int nextToken = 1;
 	
-	public Connection connect(string host, ushort port, string db = "" , string auth_key = "", int timeout = 20) {
+	public Connection connect(string host = "localhost", ushort port = 28015, string db = "" , string auth_key = "", int timeout = 20) {
 		Connection c;
 		c.sock = new Socket(AddressFamily.INET, SocketType.STREAM);
-		c.sock.setOption(SocketOptionLevel.SOCKET,SocketOption.RCVTIMEO,dur!"seconds"(timeout));
+		c.sock.setOption(SocketOptionLevel.SOCKET, SocketOption.RCVTIMEO, dur!"seconds"(timeout));
 		scope (exit) c.sock.close();
 		c.sock.connect(new InternetAddress(host,port));
 		c.sock.send("\x20\x2d\x0c\x40\x00\x00\x00\x00\xc7\x70\x69\x7e");
@@ -20,4 +22,6 @@ class Rethinkdb {
 		writefln("server said %d bytes: %s",rcv,buf[0..rcv]);
 		return c;
 	}
+
 }
+
